02b541e5659fef63c0077530b8a25ca6435b776e
reSLANE <sml37s@yahoo.com>
2017-01-21 20:18:44 -0800

    Main Menu w/ Animations

---
+++ GGJ2017Prototype/Assets/AnimateGIF.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateGIF : MonoBehaviour {
+    public Image background;
+    public TwoD[] GIFs;
+    int indexToDisplay;
+    int indexToAnimate;
+
+    int slowCounter;
+    public int slow;
+
+	// Use this for initialization
+	void Start () {
+        indexToAnimate = 0;
+        indexToDisplay = 0;
+        slowCounter = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        slowCounter++;
+        if(slowCounter%2 == 0)
+        {
+            Animate();
+        }
+    }
+    
+    void Animate()
+    {       
+        if (GIFs.Length > 0 && indexToAnimate < GIFs[Mathf.Clamp(indexToDisplay, 0, GIFs.Length - 1)].g.Length)
+        {
+            background.sprite = GIFs[indexToDisplay].g[indexToAnimate];
+            indexToAnimate++;
+        }
+        else
+        {
+            indexToAnimate = 0;
+            if(GIFs.Length > 0)
+            {
+                indexToDisplay = (indexToDisplay + 1) % (GIFs.Length);
+            }
+            
+        }
+    }
+
+    [System.Serializable]
+    public class TwoD
+    {
+        public Sprite[] g;
+
+    }
+}
---
+++ GGJ2017Prototype/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public string nextScene;
+    public GameObject loadingScreen;
+
+	// Use this for initialization
+	void Start () {
+        //Play main menu music
+        //SoundManager.i.PlaySound(Sound.Song1);
+	}
+	
+
+
+    public void StartButton()
+    {
+        loadingScreen.SetActive(true);
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
