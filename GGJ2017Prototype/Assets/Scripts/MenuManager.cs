320eda527918f65ef9b569562c5ab177de3a4a95
Tyler Pearce <pearcetyler@yahoo.com>
2017-01-21 20:43:03 -0800

    Victory Condition, Menus
    Minor bug fixes

---
+++ GGJ2017Prototype/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void Restart(){
+		SceneManager.LoadScene ("level_one");
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
---
+++ GGJ2017Prototype/Assets/Scripts/Victory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour {
+
+	public GameObject victoryScreen;
+
+
+
+	void OnCollisionEnter2D(Collision2D other){
+		victoryScreen.SetActive (true);
+	}
+}
