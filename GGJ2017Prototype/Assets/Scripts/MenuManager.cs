1a5e723cec958673b85a9f410621937338f4ef2d
reSLANE <sml37s@yahoo.com>
2017-01-22 13:03:50 -0800

    Next Level Screen Added

--- GGJ2017Prototype/Assets/Scripts/MenuManager.cs
+++ GGJ2017Prototype/Assets/Scripts/MenuManager.cs
@@ -6,13 +6,26 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour {
 
     public SoundManager soundScript;
+    string[] scenes = new string[] { "level_one 1", "level_one 2", "level_one"};
+    int currentScene = 0;
 
-	public void Restart(){
+	public void Restart()
+    {
         soundScript.EndSoundAbrupt("GGJ 17_Waves_v1");
-		SceneManager.LoadScene ("level_one");
+		SceneManager.LoadScene (scenes[currentScene]);
 	}
 
-	public void Quit(){
+    public void nextLevel()
+    {
+        soundScript.EndSoundAbrupt("GGJ 17_Waves_v1");
+        currentScene++;
+        if(currentScene >= scenes.Length)
+            SceneManager.LoadScene("main_menu");
+        SceneManager.LoadScene(scenes[currentScene + 2]);
+    }
+
+	public void Quit()
+    {
 		Application.Quit ();
 	}
 }
